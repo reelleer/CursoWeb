ea428c73db04053bba495dc7e4bcfc49353ca2c3
Rolando Espinosa Lazo <reel_leer@hotmail.com>
2015-08-20 17:02:44 -0600

    12.1 Advanced Entity Framework Scenarios
    AsNoTracking

--- asp.net/ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
+++ asp.net/ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
@@ -65,6 +65,8 @@ namespace ContosoUniversity.Controllers
                     break;
             }
 
+            students = students.AsNoTracking();
+
             int pageSize = 3;
             int pageNumber = (page ?? 1);
             return View(students.ToPagedList(pageNumber, pageSize));
