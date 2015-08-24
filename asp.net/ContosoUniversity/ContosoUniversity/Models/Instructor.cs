b4a7b5c60b5bd48e1b35b70e97a267b6ee635af9
Rolando Espinosa Lazo <reel_leer@hotmail.com>
2015-08-24 14:09:57 -0600

    Revert "13.2 Datepicker"
    This reverts commit 3b828b6b1d6d7d7efbcf8e0f625ea89605002612.

--- asp.net/ContosoUniversity/ContosoUniversity/Models/Instructor.cs
+++ asp.net/ContosoUniversity/ContosoUniversity/Models/Instructor.cs
@@ -8,8 +8,7 @@ namespace ContosoUniversity.Models
     public class Instructor : Person
     {
         [DataType(DataType.Date)]
-        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Hire Date")]
         public DateTime HireDate { get; set; }
 
