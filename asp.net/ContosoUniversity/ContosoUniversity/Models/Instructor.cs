0f610059f1b836040dac00c6491bf3f1e417d4e5
Rolando Espinosa Lazo <reel_leer@hotmail.com>
2015-08-24 14:11:12 -0600

    Revert "Revert "13.2 Datepicker""
    This reverts commit b4a7b5c60b5bd48e1b35b70e97a267b6ee635af9.

--- asp.net/ContosoUniversity/ContosoUniversity/Models/Instructor.cs
+++ asp.net/ContosoUniversity/ContosoUniversity/Models/Instructor.cs
@@ -8,7 +8,8 @@ namespace ContosoUniversity.Models
     public class Instructor : Person
     {
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         [Display(Name = "Hire Date")]
         public DateTime HireDate { get; set; }
 
---
+++ asp.net/ContosoUniversity/ContosoUniversity/Scripts/form_plugin.js
@@ -0,0 +1,29 @@
+﻿(function ($) {
+    //garatizando que las variables esten en el function scope
+    var items;
+
+    //http://eternicode.github.io/bootstrap-datepicker/
+    //Configurando los "datepickers"
+    var items = $('.calendar');
+    if (items.length > 0 && !Modernizr.inputtypes.date) {
+        items.each(function (i) {
+            var obj = $(this);
+            var fecha = obj.val();
+            if (fecha !== "") {
+                fecha = fecha
+                    .split("-")
+                    .reverse()
+                    .join("/");
+                obj.val(fecha);
+            }
+
+            var opts = {
+                autoclose: true,
+                language: 'es'
+            };
+
+            obj.datepicker(opts);
+        });
+    }
+
+})(window.jQuery);
